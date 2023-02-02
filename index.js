829a20eea1ddfc6430be059b1a35a227e29c57c5
Oladimeji Odusote <taiwo.odusote@yahoo.com>
2023-02-01 18:58:59 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(arr, event){
+    let newArr = []
+    for (let i = 0; i < arr.length; i++){
+        let message = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`;
+        newArr.push(message);
+    }
+    return newArr;
+}
+
+function countDown(num){
+    let count = num;
+    for (let i = 0; i <= num; i++){
+        console.log(count);
+        count --;
+    }
+}
